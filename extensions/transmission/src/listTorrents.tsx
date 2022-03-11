b7c13a2ccae30240b96320699807b5445e3c48e5
Federico Zivolo <federico.zivolo@gmail.com>
2022-03-11 15:07:12 +0100

    feat: side panel details and store metadata

--- extensions/transmission/src/listTorrents.tsx
+++ extensions/transmission/src/listTorrents.tsx
@@ -13,6 +13,9 @@ import { useInterval, useStateFromLocalStorage } from "./utils/hooks";
 import { capitalize, truncate } from "./utils/string";
 import { padList } from "./utils/list";
 import { createClient } from "./modules/client";
+import { renderPieces } from "./utils/renderCells";
+import { writeFileSync } from "fs";
+import dedent from "dedent-js";
 
 enum TorrentStatus {
   Stopped = 0,
@@ -34,6 +37,15 @@ const TorrentStatusLabel: Record<string, string> = {
   Seeding: "Seeding",
 };
 
+const formatDate = (date: number) =>
+  new Date(date * 1000).toLocaleString([], {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+
 type Torrent = {
   id: number;
   torrentFile: string;
@@ -45,7 +57,26 @@ type Torrent = {
   status: TorrentStatus;
   rateDownload: number;
   rateUpload: number;
-  files: { name: string }[];
+  files: { name: string; bytesCompleted: number; length: number }[];
+  pieces: string;
+  pieceCount: number;
+  pieceSize: number;
+  hashString: string;
+  creator: string;
+  dateCreated: number;
+  downloadDir: string;
+  isPrivate: boolean;
+  trackers: { announce: string; scrape: string; id: number; tier: number }[];
+  trackerStats: {
+    tier: number;
+    host: string;
+    announce: string;
+    leecherCount: number;
+    seederCount: number;
+    lastScrapeTime: number;
+    lastAnnounceTime: number;
+    downloadCount: number;
+  }[];
 };
 
 type SessionStats = {
@@ -57,6 +88,71 @@ type SessionStats = {
 };
 
 const preferences = getPreferenceValues();
+
+const NL = "  \n";
+
+const printTorrentDetails = (torrent: Torrent, downloadStats: string) => {
+  const img = renderPieces(torrent.pieces, torrent.pieceCount);
+
+  writeFileSync("/tmp/svg", img);
+
+  return dedent(`
+    ## Torrent Information
+
+    **Pieces**: ${torrent.pieceCount}, ${prettyBytes(torrent.pieceSize)}
+    **Hash**: ${torrent.hashString}  
+    **Private**: ${torrent.isPrivate ? "Yes" : "No"}
+    **Creator**: ${torrent.creator}
+    **Created On**: ${formatDate(torrent.dateCreated)}
+    **Download dir**: ${torrent.downloadDir}
+    **Comment**:
+    ${torrent.comment}
+
+    ## Transfer
+
+    ${downloadStats}
+
+    <img src="data:image/svg+xml;base64,${Buffer.from(img).toString("base64")}" />
+
+    ## Files
+
+    ${torrent.files
+      .map(
+        (file) =>
+          `- ${file.name} (_${((100 / file.length) * file.bytesCompleted).toFixed(2)}% - ${prettyBytes(file.length)}_)`
+      )
+      .join("\n")}
+
+    ## Trackers
+
+    ${torrent.trackers
+      .reduce<Array<Torrent["trackerStats"]>>((acc, tracker) => {
+        acc[tracker.tier] = (acc[tracker.tier] || []).concat(
+          torrent.trackerStats.find(({ announce }) => announce === tracker.announce) as Torrent["trackerStats"][0]
+        );
+        return acc;
+      }, [])
+      .map((trackers) => {
+        return [
+          `### Tier ${trackers[0].tier + 1}`,
+
+          ...trackers.map((tracker) =>
+            dedent(`
+                - **${tracker.host}**
+                  Last Announce: ${formatDate(tracker.lastAnnounceTime)}
+                  Last Scape: ${formatDate(tracker.lastScrapeTime)}
+                  Seeders: ${tracker.seederCount}
+                  Leechers: ${tracker.leecherCount}
+                  Downloaded: ${tracker.downloadCount}
+              `)
+          ),
+        ].join("\n");
+      })
+      .join("\n")}
+  `)
+    .split("\n")
+    .join(NL);
+};
 
 const statusToLabel = (status: TorrentStatus, percentDone: number) => {
   switch (status) {
@@ -190,6 +286,7 @@ export default function TorrentList() {
   const [torrents, setTorrents] = useState<Torrent[]>([]);
   const [sessionStats, setSessionStats] = useState<SessionStats | null>(null);
   const [didLoad, setDidLoad] = useState(false);
+  const [isShowingDetail, setIsShowingDetail] = useState(false);
 
   const updateData = useCallback(async () => {
     try {
@@ -226,6 +323,7 @@ export default function TorrentList() {
 
   return (
     <List
+      isShowingDetail={isShowingDetail}
       isLoading={!didLoad || loadingStatusFilter}
       searchBarPlaceholder="Filter torrents by name..."
       onSearchTextChange={setSearch}
@@ -256,6 +354,8 @@ export default function TorrentList() {
             rateUpload={paddedRateUploads[index]}
             percentDone={paddedPercentDones[index]}
             sessionStats={sessionStats}
+            isShowingDetail={isShowingDetail}
+            onToggleDetail={() => setIsShowingDetail((value) => !value)}
             onStop={async (torrent) => {
               try {
                 await transmission.stop([torrent.id]);
@@ -312,6 +412,8 @@ function TorrentListItem({
   onRemove,
   onStartAll,
   onStopAll,
+  onToggleDetail,
+  isShowingDetail,
   rateDownload,
   rateUpload,
   percentDone,
@@ -323,6 +425,8 @@ function TorrentListItem({
   onStartAll: (torrent: Torrent) => Promise<void>;
   onStopAll: (torrent: Torrent) => Promise<void>;
   onRemove: (torrent: Torrent, deleteLocalData: boolean) => Promise<void>;
+  onToggleDetail: () => void;
+  isShowingDetail: boolean;
   rateDownload: string;
   rateUpload: string;
   percentDone: string;
@@ -338,6 +442,8 @@ function TorrentListItem({
     .filter(Boolean)
     .join(" - ");
 
+  const downloadStats = [`↓ ${rateDownload}`, " - ", `↑ ${rateUpload}`, " - ", percentDone].join(" ");
+
   return (
     <List.Item
       id={String(torrent.id)}
@@ -347,12 +453,11 @@ function TorrentListItem({
         source: statusIconSource(torrent),
         tintColor: statusIconColor(torrent),
       }}
-      accessoryTitle={[`↓ ${rateDownload}`, " - ", `↑ ${rateUpload}`, " - ", percentDone].join(" ")}
-      detail={
-        <List.Item.Detail markdown="![Illustration](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png)" />
-      }
+      accessoryTitle={!isShowingDetail ? downloadStats : undefined}
+      detail={isShowingDetail && <List.Item.Detail markdown={printTorrentDetails(torrent, downloadStats)} />}
       actions={
         <ActionPanel>
+          <Action title={isShowingDetail ? "Hide details" : "Show details"} onAction={onToggleDetail} />
           <ActionPanel.Section title={`Selected Torrent (${selectedTorrentTitle})`}>
             <Action
               title={torrent.status === TorrentStatus.Stopped ? "Start Torrent" : "Stop Torrent"}
